361d5a7a41955bf27df424c8fb1894e5f10b0ede
edelBCS <edelm8@gmail.com>
2019-06-27 23:22:51 -0700

    added words array

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+const words = [
+    "zombie",
+    "walkers",
+    "brains",
+    "undead",
+    "unearth",
+    "graves",
+    "headshot",
+    "doubletap",
+    "cardio",
+    "limberup",
+    "cannibalize",
+    "corpse",
+    "apocalypse",
+    "reanimate",
+    "infection"]
+
+
