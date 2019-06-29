9700541a614daee5fb2947121f359ed0995e6d50
edelBCS <edelm8@gmail.com>
2019-06-29 10:27:17 -0700

    added logic to see if user won

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,6 +16,9 @@ const words = [
     "infection"
 ]
 
+//You haven't won yet
+var result = false;
+
 // Picks a mystery word from the array
 var mysteryWord = words[Math.floor(Math.random() * words.length)];
 console.log(mysteryWord);
@@ -23,22 +26,34 @@ console.log(mysteryWord);
 // Get the 'mysteryWord' element and hides the word
 var invisibleWord = document.getElementById("mysteryWord");
 var hiddenWord = hideWord(mysteryWord);
+//Displays the word as blanks spaces
 invisibleWord.textContent = hiddenWord;
 
+//when key is pressed
 document.onkeyup = function(e){
     var userGuess = e.key;
     var usedLetters = document.getElementById("usedLetters");
+
+    //if correct guess display else add to used letters pile
     (mysteryWord.indexOf(e.key) >= 0) ? (hiddenWord = unhideLetter(hiddenWord, e.key)) : (usedLetters.textContent = usedLetters.textContent + e.key + " ");
+    //displays new mystery word with correct guess
     invisibleWord.textContent = hiddenWord;
 
+    
+    result = checkWin(hiddenWord);
+    console.log("did you win: " + result);
+    (result === true)?(alert("YOU WIN!")) : "";
+    return;
 }
 
+//masks the word as underscores
 function hideWord(word){
     var hiddenWord = "";
     for(var letters of word){hiddenWord = hiddenWord + "_"}
     return hiddenWord;
 }
 
+//checks if letter is in word, reveals it if correct
 function unhideLetter(hiddenWord, letter){
     
     for(var i = 0; i<mysteryWord.length; ++i){
@@ -49,4 +64,11 @@ function unhideLetter(hiddenWord, letter){
     
     console.log(hiddenWord);
     return  hiddenWord;
+}
+
+//checks to see if user won
+function checkWin(hiddenWord){
+    var result = (hiddenWord.indexOf("_") >= 0) ? false : true;
+    
+    return result;
 }
